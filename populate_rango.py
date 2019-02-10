dc341cf015cfc067a67f22965a8e6da6ee77feb9
Nadya Barlow <nadyabarlow@hotmail.com>
2019-02-10 15:17:36 +0000

    Fixes

--- populate_rango.py
+++ populate_rango.py
@@ -8,7 +8,7 @@ django.setup() # Import Django project's settings
 from rango.models import Category, Page
 
 def populate():
-    # First, we will create lists of dictionaries containing the pages
+    # First, we will create lists of dictionarie containing the pages
     # we want to add into each category.
     # Then we will create a dictionary of dictionaries for our categories.
     # This might seem a little bit confusing, but it allows us to iterate
@@ -16,27 +16,35 @@ def populate():
 
     python_pages = [
         {"title" : "Official Python Tutorial",
-         "url" : "http://docs.python.org/2/tutorial/"},
+         "url" : "http://docs.python.org/2/tutorial/",
+         "pviews" : "54"},
         {"title" : "How to Think like a Computer Scientist",
-         "url" : "http://www.greenteapress.com/thinkpython/"},
+         "url" : "http://www.greenteapress.com/thinkpython/",
+         "pviews" : "67"},
         {"title" : "Learn Python in 10 Minutes",
-         "url" : "http://www.korokithakis.net/tutorials/python/"}
+         "url" : "http://www.korokithakis.net/tutorials/python/",
+         "pviews" : "23"}
     ]
 
     django_pages =[
         {"title" : "Official Django Tutorial",
-         "url" : "https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+         "url" : "https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+         "pviews" : "115"},
         {"title" : "Django Rocks",
-         "url" : "http://www.djangorocks.com/"},
+         "url" : "http://www.djangorocks.com/",
+         "pviews" : "675"},
         {"title" : "How to Tango with Django",
-         "url" : "http://www.tangowithdjango.com/"}
+         "url" : "http://www.tangowithdjango.com/",
+         "pviews" : "6789"}
     ]
 
     other_pages = [
         {"title" : "Bottle",
-         "url" : "http://bottlepy.org/docs/dev/"},
+         "url" : "http://bottlepy.org/docs/dev/",
+         "pviews" : "43"},
         {"title" : "Flask",
-         "url" : "http://flask.pocoo.org"}
+         "url" : "http://flask.pocoo.org",
+         "pviews" : "22"}
     ]
 
     cats = {"Python" : {"pages" : python_pages, "views" : 128, "likes" : 64},
@@ -46,13 +54,13 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data["views"], cat_data["likes"]) # Do category first as page needs category ref.
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["pviews"])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print( "- {0} - {1}". format(str(c), str(p)))
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
     p.views=views
--- rango/views.py
+++ rango/views.py
@@ -13,22 +13,17 @@ from django.contrib.auth import logout
 from datetime import datetime
 
 def index(request):
-    # Query the database for a list of ALL categories currently stored.
-    # Order the categories by no. likes in descending order.
-    # Retrieve the top 5 only - or all if less than 5.
-    # Place the list in our context_dict dictionary
-    # that will be passed to the template engine.
-    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-    context_dict = {'categories' : category_list, 'pages' : page_list}
+    context_dict = {'categories': category_list, 'pages': page_list}
 
+    # Obtain our Response object early so we can add cookie information.
+    response = render(request, 'rango/index.html', context_dict)
 
-    visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
+    # Call the helper function to handle the cookies
+    visitor_cookie_handler(request, response)
 
-    response = render(request, 'rango/index.html', context=context_dict)
-    # Render the response and send it back!
+    # Return response back to the user, updating any cookies that need changed.
     return response
 
 def about(request):
@@ -189,14 +184,24 @@ def get_server_side_cookie(request, cookie, default_val=None):
         val = default_val
     return val
 
-def visitor_cookie_handler(request):
-    visits = int(get_server_side_cookie(request, 'visits', '1'))
-    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
-    if(datetime.now() - last_visit_time).days > 0:
-        visits = visits + 1
-        request.session['last_visit'] = str(datetime.now())
-    else:
-        request.session['last_visit'] = last_visit_cookie
+def visitor_cookie_handler(request, response):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(request.COOKIES.get('visits', '1'))
 
-    request.session['visits'] = visits
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        # Set the last visit cookie
+        response.set_cookie('last_visit', last_visit_cookie)
+
+    # Update/set the visits cookie
+    response.set_cookie('visits', visits)
